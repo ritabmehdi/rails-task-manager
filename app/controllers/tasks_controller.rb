c0a0da23adacdc60a78a1560649bfed8d4da64d3
Mehdi Ritab <ritabmehdi@gmail.com>
2018-11-22 17:41:57 +0100

    commiting all files of rails-task-manager challenge's

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,52 @@
 class TasksController < ApplicationController
-
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
+  end
+
+  def show
+    #@task = Task.find(params[:id])
+    #rails
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    #@task = Task.create(title: params["task"]["title"], details: params["task"]["details"], completed: false)
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    #@task = Task.find(params[:id])
+  end
+
+  def update
+    #raise
+    #@task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    #raise
+    #@task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  resources :tasks do
+
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  #add a new route to list the tasks
-  get 'tasks', to: 'tasks#index'
+  # #add a new route to list the tasks
+  # get 'tasks', to: 'tasks#index'
+
+  # get 'tasks/new', to: 'tasks#new', as: 'newtask'
+  # post 'tasks/', to: 'tasks#create', as: 'createtask'
+
+  # get 'tasks/:id', to: 'tasks#show', as: 'taskprefix' #we can use show_path()
+
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'editprefix'
+  # patch 'tasks/:id', to: 'tasks#update', as: 'updateprefix'
 end
